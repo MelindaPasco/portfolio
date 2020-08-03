518f91aaf73ca2ff97986b9b8602741555bc107e
MelindaPasco <66061324+MelindaPasco@users.noreply.github.com>
2020-08-03 18:02:29 +0200

    Portfolio

---
+++ script.js
@@ -0,0 +1,23 @@
+var likes = ['dessiner crayon-papier',
+            'jouer au volley', 
+            'rire',
+            'les jeux video',
+            "m'amuser avec le CSS",
+            'la science-fiction', 
+            'le fantastique',
+            'relever des challenges'];
+
+textSequence(0);
+
+function textSequence(i) {
+
+    if (likes.length > i) {
+        setTimeout(function() {
+            document.getElementById("sequence").innerHTML = likes[i];
+            textSequence(++i);
+        }, 2500);
+
+    } else if (likes.length == i) {
+        textSequence(0);
+    }
+}
